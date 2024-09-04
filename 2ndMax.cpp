0b4a2a00944b1cb09759ceb50518f128a91e6702
Addya Pandey <123183433+AddyaPandey@users.noreply.github.com>
2024-09-04 21:41:08 +0530

    array

---
+++ 2ndMax.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+
+        cin >> arr[i];
+    }
+    int mx = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i]>mx){
+            mx=arr[i];
+        }
+    }
+    int second_max = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i] != mx){
+
+        if(arr[i]>second_max)
+            second_max = arr[i];
+        }
+    }
+}
